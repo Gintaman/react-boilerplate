1f715edd1e8cd1925101e7365fdd0d579d4dfdde
Jeff Chen <mechazawa@pop-os.localdomain>
2023-03-11 01:17:49 -0800

    add basic configs

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+import spongebob from "./spongebob.png";
+
+export const App = () => {
+    return (
+        <>
+            <h1 className="wtf">React Template</h1>
+            <img src={spongebob} alt="spongebob" />
+        </>
+    );
+};
